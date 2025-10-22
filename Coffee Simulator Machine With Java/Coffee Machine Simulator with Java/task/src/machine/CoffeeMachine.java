98ff43550e265b9beaf7a2a6a87d514111922451
Rebakure <kevinrebakure@gmail.com>
2025-10-22 17:14:16 +0200

    estimate the number of servings

--- Coffee Simulator Machine With Java/Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Simulator Machine With Java/Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -1,30 +1,54 @@
 package machine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String[] args) {
-        final int mlOfWaterPerCup = 200;
-        final int mlOfMilkPerCup = 50;
-        final int gOfCoffeeBeansPerCup = 15;
+    static final int mlOfWaterPerCup = 200;
+    static final int mlOfMilkPerCup = 50;
+    static final int gOfCoffeeBeansPerCup = 15;
 
+    public static void main(String[] args) {
         Scanner readInput = new Scanner(System.in);
 
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int waterCapacityInml = readInput.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milkCapacityInml = readInput.nextInt();
+
+        System.out.println("Write how many grams of coffee beans coffee machine has: ");
+        int coffeeBeansCapacityIng = readInput.nextInt();
+
         System.out.println("Write how many cups of coffee you will need: ");
-        int cups = readInput.nextInt();
+        int cupsRequested = readInput.nextInt();
 
-        System.out.printf("For %d cups of coffee you will need: ", cups);
-        System.out.println();
+        int capacity = calculateNumberOfCupsToMake(waterCapacityInml, milkCapacityInml, coffeeBeansCapacityIng);
 
-        System.out.printf("%d ml of water", cups * mlOfWaterPerCup);
-        System.out.println();
-
-        System.out.printf("%d ml of milk", cups * mlOfMilkPerCup);
-        System.out.println();
-
-        System.out.printf("%d g of coffee beans", cups * gOfCoffeeBeansPerCup);
-        System.out.println();
+        if (capacity > cupsRequested){
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)%n", capacity - cupsRequested);
+            System.out.println();
+        } else if (capacity < cupsRequested) {
+            System.out.printf("No, I can make only %d cup(s) of coffee", capacity);
+            System.out.println();
+        } else {
+            System.out.println("Yes, I can make that amount of coffee");
+        }
 
         readInput.close();
+    }
+
+    public static int calculateNumberOfCupsToMake(
+            int water,
+            int milk,
+            int beans) {
+
+        int waterCups = water / mlOfWaterPerCup;
+        int milkCups = milk / mlOfMilkPerCup;
+        int beansGrams = beans / gOfCoffeeBeansPerCup;
+
+        int[] amounts = {waterCups, milkCups, beansGrams};
+
+        return Arrays.stream(amounts).min().getAsInt();
     }
 }
