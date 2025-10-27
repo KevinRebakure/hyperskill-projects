f7098b99d03a382633a236f25454e7088bf6e4dd
Rebakure <kevinrebakure@gmail.com>
2025-10-27 21:08:08 +0200

    task 2: measuring total income

--- My First Project with Java/My First Project with Java/task/src/calculator/Main.java
+++ My First Project with Java/My First Project with Java/task/src/calculator/Main.java
@@ -1,13 +1,30 @@
 package calculator;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prices:");
-        System.out.println("Bubblegum: $2");
-        System.out.println("Toffee: $0.2");
-        System.out.println("Ice cream: $5");
-        System.out.println("Milk chocolate: $4");
-        System.out.println("Doughnut: $2.5");
-        System.out.println("Pancake: $3.2");
+        Map<String, Integer> items = Map.of(
+                "Bubblegum", 202,
+                "Toffee", 118,
+                "Ice cream", 2250,
+                "Milk chocolate", 1680,
+                "Doughnut", 1075,
+                "Pancake", 80
+        );
+
+        System.out.println("Earned amount:");
+        System.out.println("Bubblegum: $202");
+        System.out.println("Toffee: $118");
+        System.out.println("Ice cream: $2250");
+        System.out.println("Milk chocolate: $1680");
+        System.out.println("Doughnut: $1075");
+        System.out.println("Pancake: $80");
+        System.out.println();
+
+        int sum = items.values().stream().mapToInt(Integer::intValue).sum();
+
+        System.out.printf("Income: $%d", sum);
+        System.out.println();
     }
 }
