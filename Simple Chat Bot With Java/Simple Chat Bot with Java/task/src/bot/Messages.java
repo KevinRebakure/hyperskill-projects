e08394a70f8436f6b7e5a59ee479744b75117c84
Rebakure <kevinrebakure@gmail.com>
2025-10-29 08:31:58 +0200

    introduce yourself

--- Simple Chat Bot With Java/Simple Chat Bot with Java/task/src/bot/Messages.java
+++
@@ -1,8 +0,0 @@
-package bot;
-
-public class Messages {
-    public void greet() {
-        System.out.println("Hello! My name is Aid.");
-        System.out.println("I was created in 2023");
-    }
-}
