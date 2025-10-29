15941e48f628dde5c8a7edbe0301d66b9ed12f3b
Rebakure <kevinrebakure@gmail.com>
2025-10-29 10:49:55 +0200

    task 2: the user is the gamemaster

--- Simple Tic-Tac-Toe (Java)/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,9 +1,29 @@
 package tictactoe;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String[] row1 = input.substring(0, 3).split("");
+        String[] row2 = input.substring(3, 6).split("");
+        String[] row3 = input.substring(6, 9).split("");
+
+        List<String[]> matrix = List.of(row1, row2, row3);
+
+        System.out.println("-".repeat(9));
+        for (String[] row : matrix) {
+            System.out.print("| ");
+            System.out.print(String.join(" ", row));
+            System.out.print(" |\n");
+        }
+
+        System.out.println("-".repeat(9));
+
+        scanner.close();
     }
 }
