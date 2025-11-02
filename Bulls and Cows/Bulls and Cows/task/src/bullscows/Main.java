ea3db93116fada6e32f0bb407f773303ca1898cb
Rebakure <kevinrebakure@gmail.com>
2025-11-02 13:59:29 +0200

    task 3: secret code

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -1,14 +1,57 @@
 package bullscows;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final String secret  = "9305";
-
         Scanner scanner = new Scanner(System.in);
 
-        String guess = scanner.nextLine();
+        int lengthOfSecret = scanner.nextInt();
+
+        if (lengthOfSecret >= 11) {
+            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.\n");
+            return;
+        }
+
+        String secret = generateSecret(lengthOfSecret);
+        System.out.printf("the random secret number is %s.\n", secret);
+
+//        String guess = scanner.nextLine();
+//        gradeGuess(guess, secret);
+
+        scanner.close();
+    }
+
+    private static String generateSecret(int lengthOfSecret) {
+        StringBuilder secret = new StringBuilder();
+
+        while (secret.length() < lengthOfSecret) {
+            long pseudoRandomNumber = System.nanoTime();
+            String[] randomNumberDigits = String.valueOf(pseudoRandomNumber).split("");
+
+            for (int i = randomNumberDigits.length - 1; i >= 0; i--) {
+                String currentDigit = randomNumberDigits[i];
+
+                if (secret.isEmpty() && currentDigit.equals("0")) {
+                    continue;
+                }
+
+                if (!secret.toString().contains(currentDigit) && Character.isDigit(currentDigit.charAt(0))) {
+                    secret.append(currentDigit);
+                }
+
+                if (secret.length() == lengthOfSecret) {
+                    break;
+                }
+            }
+        }
+
+        return secret.toString();
+    }
+
+
+    private static void gradeGuess(String guess, String secret) {
         String[] digits = guess.split("");
 
         int bulls = 0;
@@ -34,7 +77,5 @@ public class Main {
         } else {
             System.out.printf("Grade: None. The secret code is %s.", secret);
         }
-
-        scanner.close();
     }
 }
