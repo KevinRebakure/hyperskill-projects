604e7c85a0f0aef45ae002c4556a8eee9db5ea86
Rebakure <kevinrebakure@gmail.com>
2025-11-02 13:10:22 +0200

    feat: get input for x

--- Simple Tic-Tac-Toe (Java)/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,8 +1,7 @@
 package tictactoe;
 
-import java.util.List;
+import java.util.*;
 import java.util.Objects;
-import java.util.Scanner;
 
 public class Main {
     private static boolean xWins = false;
@@ -11,25 +10,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Provide initial ");
+        System.out.print("Enter cells: ");
         String input = scanner.nextLine();
 
         // rows
-        List<String> row1 = List.of(input.substring(0, 3).split(""));
-        List<String> row2 = List.of(input.substring(3, 6).split(""));
-        List<String> row3 = List.of(input.substring(6, 9).split(""));
+        List<String> row1 = Arrays.asList(input.substring(0, 3).split(""));
+        List<String> row2 = Arrays.asList(input.substring(3, 6).split(""));
+        List<String> row3 = Arrays.asList(input.substring(6, 9).split(""));
 
         // matrix
         List<List<String>> matrix = List.of(row1, row2, row3);
 
-        System.out.println("-".repeat(9));
-        for (List<String> row : matrix) {
-            System.out.print("| ");
-            System.out.print(String.join(" ", row));
-            System.out.print(" |\n");
-        }
-        System.out.println("-".repeat(9));
+        printMatrix(matrix);
 
+        System.out.print("Enter the coordinates: ");
+        getInput(matrix, scanner);
+
+        printMatrix(matrix);
+
+        /* Commented out for this stage - will be used in final stage
         int countX = (int) matrix.stream().flatMap(List::stream).filter(x -> Objects.equals(x, "X")).count();
         int countO = (int) matrix.stream().flatMap(List::stream).filter(x -> Objects.equals(x, "O")).count();
         int countSpaces = (int) matrix.stream().flatMap(List::stream).filter(x -> Objects.equals(x, "_")).count();
@@ -68,7 +67,46 @@ public class Main {
         }
 
         System.out.println("Game not finished");
+        */
         scanner.close();
+    }
+
+    public static void getInput(List<List<String>> matrix, Scanner scanner) {
+        boolean placed = false;
+        while (!placed) {
+            try {
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
+
+                if (x < 1 || x > 3 || y < 1 || y > 3) {
+                    System.out.println("Coordinates should be from 1 to 3!");
+                    continue;
+                }
+
+                String selectedCell = matrix.get(x - 1).get(y - 1);
+
+                if (!selectedCell.equals("_")) {
+                    System.out.println("This cell is occupied! Choose another one!");
+                    continue;
+                }
+
+                matrix.get(x - 1).set(y - 1, "X");
+                placed = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter numbers!");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static void printMatrix(List<List<String>> matrix) {
+        System.out.println("-".repeat(9));
+        for (List<String> row : matrix) {
+            System.out.print("| ");
+            System.out.print(String.join(" ", row));
+            System.out.print(" |\n");
+        }
+        System.out.println("-".repeat(9));
     }
 
     public static void checkRows(List<List<String>> matrix) {
