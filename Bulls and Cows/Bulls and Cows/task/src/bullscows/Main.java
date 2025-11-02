f2b46ba00efa812902b8b9ef95b93d29e14219e8
Rebakure <kevinrebakure@gmail.com>
2025-11-02 13:37:25 +0200

    task 2: grader

--- Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -1,16 +1,40 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****.\n");
-        System.out.println("Turn 1. Answer:");
-        System.out.println("1234");
-        System.out.println("Grade: 1 cow.");
+        final String secret  = "9305";
 
-        System.out.println("Turn 2. Answer:");
-        System.out.println("5678");
-        System.out.println("Grade: 1 cow.");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Congrats! The secret code is 9305.\n");
+        String guess = scanner.nextLine();
+        String[] digits = guess.split("");
+
+        int bulls = 0;
+        int cows = 0;
+
+        for (int i = 0; i < digits.length; i++) {
+            String currentDigit = digits[i];
+            if (secret.contains(currentDigit)) {
+                if (secret.indexOf(currentDigit) == i) {
+                    bulls += 1;
+                } else {
+                    cows += 1;
+                }
+            }
+        }
+
+        if (bulls > 0 && cows > 0) {
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.", bulls, cows, secret);
+        } else if (bulls > 0) {
+            System.out.printf("Grade: %d bull(s). The secret code is %s.", bulls, secret);
+        } else if (cows > 0) {
+            System.out.printf("Grade: %d cow(s). The secret code is %s.", cows, secret);
+        } else {
+            System.out.printf("Grade: None. The secret code is %s.", secret);
+        }
+
+        scanner.close();
     }
 }
