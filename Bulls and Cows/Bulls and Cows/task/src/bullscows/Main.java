bc588ca27a6832187fa2a6fcfe6b93fb9f5fd41f
Rebakure <kevinrebakure@gmail.com>
2025-11-02 13:27:17 +0200

    task 1: Game log

---
+++ Bulls and Cows/Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,16 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The secret code is prepared: ****.\n");
+        System.out.println("Turn 1. Answer:");
+        System.out.println("1234");
+        System.out.println("Grade: 1 cow.");
+
+        System.out.println("Turn 2. Answer:");
+        System.out.println("5678");
+        System.out.println("Grade: 1 cow.");
+
+        System.out.println("Congrats! The secret code is 9305.\n");
+    }
+}
